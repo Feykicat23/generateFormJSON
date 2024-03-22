c0243338560c7848ed894d93255dc5deb55e1d2b
Arseniy Koshkin <arseniy.koshkin@mail.ru>
2024-03-22 13:15:24 +0200

    unique radioID and new form-config

--- src/components/form-items/FormRadio.vue
+++ src/components/form-items/FormRadio.vue
@@ -1,18 +1,18 @@
 <template>
-  <fieldset>
+  <fieldset :id="formId">
     <legend>{{ label }}</legend>
 
     <div v-for="option in options" :key="option.value">
       <input 
         required 
         type="radio" 
-        name="radio"
-        :id="option.value"
+        :name="radioName"
+        :id="generateId(option.value)"
         :value="option.value"
         :checked="option.value === value"
         @change="yearsChange(parseInt(option.value))"
       />
-      <label :for="option.value">{{ option.text }}</label>
+      <label :for="generateId(option.value)">{{ option.text }}</label>
     </div>
   </fieldset>
 </template>
@@ -36,10 +36,19 @@ export default {
     }
   },
 
+  data() {
+    return {
+      formId: 'form-' + Math.random().toString(36),
+      radioName: 'radio-' + Math.random().toString(36)
+    };
+  },
+
   methods: {
     yearsChange(years) {
-     
-      this.$emit('years', years)
+      this.$emit('years', years);
+    },
+    generateId(value) {
+      return `radio-${this.formId}-${value}`;
     }
   }
 }
